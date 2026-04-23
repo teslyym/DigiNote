0c37e84cb2516347115be8e575dc485797a824ff
Teslim <steslimolayiwola@gmail.com>
2026-04-23 05:50:02 +0100

    Remove unnecessary whitespace in AppLayout component and add new Reminders page

--- Client/src/Components/AppLayout.jsx
+++ Client/src/Components/AppLayout.jsx
@@ -6,7 +6,6 @@ const AppLayout = () => {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <Sidebar />
-
       <main className="flex-1 p-6 md:p-8">
         <Outlet />
       </main>
